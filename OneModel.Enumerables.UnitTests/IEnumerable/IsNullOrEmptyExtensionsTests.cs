9bba89aa8cf8845ea68693156f249f5e4f197b06
Marcus Graham <marcus.graham@onemodel.co>
2016-06-29 17:26:43 +1000

    Added IsNullOrEmpty extension to IEnumerable and ICollection

---
+++ OneModel.Enumerables.UnitTests/IEnumerable/IsNullOrEmptyExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using OneModel.Enumerables.IEnumerable;
+using Xunit;
+
+namespace OneModel.Enumerables.UnitTests.IEnumerable
+{
+    public class IsNullOrEmptyExtensionsTests
+    {
+        [Fact]
+        public void Returns_True_For_Null_Enumerable()
+        {
+            var nullEnumerable = GetNullEnumerable<int>();
+            
+            var actual = nullEnumerable.IsNullOrEmpty();
+
+            Assert.Equal(true, actual);
+        }
+
+        [Fact]
+        public void Returns_True_For_Empty_Enumerable()
+        {
+            IEnumerable<int> emptyEnumerable = new List<int>();
+
+            var actual = emptyEnumerable.IsNullOrEmpty();
+
+            Assert.Equal(true, actual);
+        }
+        
+        [Fact]
+        public void Returns_False_For_Enumerable_With_Elements()
+        {
+            IEnumerable<int> nonEmptyNonNullEnurable = new List<int> { 1, 2, 3 };
+
+            var actual = nonEmptyNonNullEnurable.IsNullOrEmpty();
+
+            Assert.Equal(false, actual);
+        }
+        
+        private IEnumerable<T> GetNullEnumerable<T>()
+        {
+            return null;
+        }
+    }
+}
---
+++ OneModel.Enumerables/ICollection/IsNullOrEmptyExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace OneModel.Enumerables.ICollection
+{
+    public static class IsNullOrEmptyExtensions
+    {
+        /// <summary>
+        /// Returns true for an empty or null collection.
+        /// </summary>
+        public static bool IsNullOrEmpty<T> (this ICollection<T> collection)
+        {
+            if (collection == null)
+                return true;
+
+            return collection.Count < 1;
+        }
+    }
+}
