4f978184ac182951378bb16281ec8b8155bc9d51
Andrew Moore <andrew.moore@onemodel.co>
2016-11-11 14:19:45 +1000

    Version 1.4.0. Added IEnumerable<T>.Concat(params T[] items) extension method.

---
+++ OneModel.Enumerables/IEnumerable/ConcatExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// ReSharper disable once CheckNamespace
+namespace OneModel.Enumerables
+{
+    // ReSharper disable once InconsistentNaming
+    public static partial class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Concatenates two sequences. Similar to the built-in LINQ 
+        /// Concat(), except with a params array.
+        /// </summary>
+        public static IEnumerable<T> Concat<T>(this IEnumerable<T> source, params T[] items)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            return Enumerable.Concat(source, items);
+        }
+    }
+}
