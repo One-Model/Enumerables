c826b41bd0093ec96eee5127f0c670495189a01c
Andrew Moore <andrew.moore@onemodel.co>
2016-12-14 11:17:04 +1000

    Version 1.6.0. Added ToReadOnlyCollection() extension method.

---
+++ OneModel.Enumerables/IEnumerable/ToReadOnlyCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OneModel.Enumerables.IEnumerable
+{
+    public static class ToReadOnlyCollectionExtensions
+    {
+        /// <summary>
+        /// Converts an enumerable to an IReadOnlyCollection&lt;T&gt;
+        /// Useful if you want to be able to reason about the length of
+        /// a enumerable and want to avoid multiple enumerations, and don't
+        /// care if the source is an array, list or whatever.
+        /// </summary>
+        public static IReadOnlyCollection<T> ToReadOnlyCollection<T>(this IEnumerable<T> source)
+        {
+            return source as IReadOnlyCollection<T> ?? source.ToArray();
+        }
+    }
+}
