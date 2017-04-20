9081f59058058d9736b41e98a6ad8d9f8d522c0c
Alex Crichton <alex.crichton@onemodel.co>
2017-04-20 10:43:14 +1000

    Add VennResultExtensions.LeftOrRight<T>

---
+++ OneModel.Enumerables/VennResultExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OneModel.Enumerables
+{
+    /// <summary>
+    /// Extensions to OneModel.Enumerables.VennResult&lt;TLeft, TRight&gt;
+    /// </summary>
+    public static class VennResultExtensions
+    {
+        public static IReadOnlyList<T> LeftOrRight<T>(this VennResult<T, T> venn)
+            => venn.Left.Concat(venn.Right).ToList();
+    }
+}
