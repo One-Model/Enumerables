b95823bcceb4a862f13fb91c1a0ce03f7c25e3ae
Andrew Moore <andrew.moore@onemodel.co>
2017-05-16 15:56:47 +1000

    Version 1.8.0. Add FastRemoveList.

---
+++ OneModel.Enumerables.UnitTests/RemoveOnlyCollectionTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace OneModel.Enumerables.UnitTests
+{
+    public class RemoveOnlyCollectionTests
+    {
+        [Fact]
+        public void Throws_Exception_Is_Items_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new FastRemoveList<int>(null));
+        }
+        
+        [Fact]
+        public void Items_Can_Be_Enumerated()
+        {
+            var collection = new FastRemoveList<int>(1, 2, 3);
+            Assert.Collection(collection.OrderBy(a => a),
+                i => Assert.Equal(1, i),
+                i => Assert.Equal(2, i),
+                i => Assert.Equal(3, i));
+        }
+
+        [Fact]
+        public void Items_Can_Be_Removed_From_The_End()
+        {
+            var collection = new FastRemoveList<int>(1, 2, 3);
+            collection.RemoveAt(2);
+            
+            Assert.Collection(collection,
+                i => Assert.Equal(1, i),
+                i => Assert.Equal(2, i));
+        }
+
+        [Fact]
+        public void Items_Can_Be_Removed_From_The_Start()
+        {
+            var collection = new FastRemoveList<int>(1, 2, 3);
+            collection.RemoveAt(0);
+            Assert.Collection(collection.OrderBy(a => a),
+                i => Assert.Equal(2, i),
+                i => Assert.Equal(3, i));
+        }
+        
+        [Fact]
+        public void All_Items_Can_Be_Removed()
+        {
+            var collection = new FastRemoveList<int>(1, 2, 3);
+            collection.RemoveAt(0);
+            collection.RemoveAt(0);
+            collection.RemoveAt(0);
+            Assert.Equal(0, collection.Count);
+            Assert.Collection(collection);
+        }
+
+        [Fact]
+        public void Items_Can_Be_Retrieved_By_Index()
+        {
+            var collection = new FastRemoveList<int>(1, 2, 3);
+            Assert.Equal(1, collection[0]);
+            Assert.Equal(2, collection[1]);
+            Assert.Equal(3, collection[2]);
+        }
+
+        [Fact]
+        public void Items_Can_Be_Retrieved_By_Index_After_Removing_Items()
+        {
+            var collection = new FastRemoveList<int>(1, 2, 3);
+            collection.RemoveAt(0);
+            Assert.Equal(3, collection[0]);
+            Assert.Equal(2, collection[1]);
+        }
+    }
+}
---
+++ OneModel.Enumerables/FastRemoveList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OneModel.Enumerables
+{
+    /// <summary>
+    /// An implementation of IReadOnlyList that only permits
+    /// reading and removing items. New items cannot be 
+    /// added. Supports efficient O(1) removal of items
+    /// like a linked list, but supports index-based 
+    /// retrieval of items as well. Removing items will
+    /// reorder the collection unless the item removed is 
+    /// the last item.
+    /// </summary>
+    public class FastRemoveList<T> : IReadOnlyList<T>
+    {
+        public FastRemoveList(IEnumerable<T> items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            _items = items.ToArray();
+            Count = _items.Length;
+        }
+
+        public FastRemoveList(params T[] items) : this((IEnumerable<T>)items)
+        {
+        }
+
+        private readonly T[] _items;
+
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Removes the item at the specified index.
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveAt(int index)
+        {
+            CheckBounds(index);
+
+            _items[index] = _items[Count - 1];
+            Count--;
+            _items[Count] = default(T);
+        }
+
+        private void CheckBounds(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Argument {nameof(index)} must not be less than 0.");
+            }
+
+            if (index >= Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Argument {nameof(index)} was outside the bounds of the collection.");
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                CheckBounds(index);
+                return _items[index];
+            }
+        }
+
+        IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(_items, Count);
+        }
+
+        private class Enumerator : IEnumerator<T>
+        {
+            private readonly T[] _items;
+            private readonly int _count;
+            private int _position = -1;
+
+            public Enumerator(T[] items, int count)
+            {
+                _items = items;
+                _count = count;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                return ++_position < _count;
+            }
+
+            public void Reset()
+            {
+                _position = -1;
+            }
+
+            object IEnumerator.Current => Current;
+
+            public T Current => _items[_position];
+        }
+    }
+}
