9f55b04aa7982382bc99c2a0a3e92fcf8715d7c5
Andrew Moore <andrew.moore@onemodel.co>
2017-06-14 13:27:35 +1000

    Version 1.9.0. Add IEnumerable<T>.Buffer() extension method.

---
+++ OneModel.Enumerables/BufferedEnumerable.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace OneModel.Enumerables
+{
+    /// <summary>
+    /// Wraps an enumerable, allowing it to be read multiple
+    /// times without enumerating the underlying source more
+    /// than once, and also without enumerating the underlying
+    /// source up front.
+    /// </summary>
+    public class BufferedEnumerable<T> : IEnumerable<T>
+    {
+        private readonly IList<T> _buffer;
+        private readonly IEnumerator<T> _source;
+        
+        public BufferedEnumerable(IEnumerable<T> source)
+        {
+            _source = source.GetEnumerator();
+            _buffer = new List<T>();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new BufferedEnumerator<T>(_source, _buffer);
+        }
+
+        IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// The number of items that have been read into the buffer
+        /// so far.
+        /// </summary>
+        public int NumRead => _buffer.Count;
+
+        public class BufferedEnumerator<T> : IEnumerator<T>
+        {
+            private readonly IEnumerator<T> _source;
+            private readonly IList<T> _buffer;
+            private int _index = -1;
+
+            public BufferedEnumerator(IEnumerator<T> source, IList<T> buffer)
+            {
+                _source = source;
+                _buffer = buffer;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                _index++;
+                if (_index < _buffer.Count)
+                {
+                    return true;
+                }
+
+                if (_source.MoveNext())
+                {
+                    _buffer.Add(_source.Current);
+                    return true;
+                }
+
+                return false;
+            }
+
+            public void Reset()
+            {
+                _index = -1;
+            }
+
+            public T Current => _buffer[_index];
+
+            object IEnumerator.Current => Current;
+        }
+    }
+}
---
+++ OneModel.Enumerables/IEnumerable/BufferExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OneModel.Enumerables
+{
+    public static partial class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Buffers access to an enumerable, allowing lazy read access to it, 
+        /// and ensuring that the source enumerable is read at most once.
+        /// </summary>
+        public static BufferedEnumerable<T> Buffer<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return new BufferedEnumerable<T>(source);
+        }
+    }
+}
