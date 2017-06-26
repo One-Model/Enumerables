941253d3d410e0ad44bf388a6f57af281a7a7551
Andrew Moore <andrew.moore@onemodel.co>
2017-06-26 10:11:36 +1000

    Version 1.11.0. Add IDictionary<TKey,TValue>.GetOrCreate(...) method. Update xunit nugets to latest versions.

---
+++ OneModel.Enumerables/IDictionary/GetOrCreateExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OneModel.Enumerables
+{
+    // ReSharper disable once InconsistentNaming
+    public static partial class IDictionaryExtensions
+    {
+        /// <summary>
+        /// Retrieves the item with the given key from the dictionary. If no item
+        /// is found, then the creator function will be used to create a new
+        /// instance, add it to the dictionary, and then that new instance will
+        /// be returned.
+        /// </summary>
+        public static TValue GetOrCreate<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> creator)
+        {
+            if (dictionary == null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            if (creator == null)
+            {
+                throw new ArgumentNullException(nameof(creator));
+            }
+
+            TValue existing;
+            if (!dictionary.TryGetValue(key, out existing))
+            {
+                existing = creator();
+                dictionary[key] = existing;
+            }
+
+            return existing;
+        }
+    }
+}
