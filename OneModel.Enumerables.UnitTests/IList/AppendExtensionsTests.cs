57c5c8ddc4404290822f60ac4ea56b0ee734b8da
Andrew Moore <andrew.moore@onemodel.co>
2017-07-26 13:43:12 +1000

    Version 1.12.0. Change the return types of Append() and AppendRange()  to match the input source type, rather than IList<T>.

--- OneModel.Enumerables.UnitTests/IList/AppendExtensionsTests.cs
+++ OneModel.Enumerables.UnitTests/IList/AppendExtensionsTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using Xunit;
 
 namespace OneModel.Enumerables.UnitTests.IList
@@ -33,34 +36,7 @@ namespace OneModel.Enumerables.UnitTests.IList
                 item => Assert.Equal(1, item),
                 item => Assert.Equal(2, item));
         }
-
-        [Fact]
-        public void AppendRange_Array_Throws_Exception_If_Source_Is_Null()
-        {
-            List<int> source = null;
-            Assert.Throws<ArgumentNullException>(() => source.AppendRange(0, 1));
-        }
-
-        [Fact]
-        public void AppendRange_Array_Throws_Exception_If_Items_Array_Is_Null()
-        {
-            List<int> source = new List<int>();
-            Assert.Throws<ArgumentNullException>(() => source.AppendRange((int[])null));
-        }
-
-        [Fact]
-        public void AppendRange_Array_Inserts_The_Items_At_The_End()
-        {
-            var source = new List<int> { 0, 1 };
-            source.AppendRange(2, 3);
-
-            Assert.Collection(source,
-                item => Assert.Equal(0, item),
-                item => Assert.Equal(1, item),
-                item => Assert.Equal(2, item),
-                item => Assert.Equal(3, item));
-        }
-
+        
         [Fact]
         public void AppendRange_Enumerable_Throws_Exception_If_Source_Is_Null()
         {
@@ -79,7 +55,7 @@ namespace OneModel.Enumerables.UnitTests.IList
         public void AppendRange_Enumerable_Inserts_The_Items_At_The_End()
         {
             var source = new List<int> { 0, 1 };
-            source.AppendRange(new List<int> { 2, 3 });
+            source.AppendRange(new[] { 2, 3 });
 
             Assert.Collection(source,
                 item => Assert.Equal(0, item),
@@ -88,5 +64,80 @@ namespace OneModel.Enumerables.UnitTests.IList
                 item => Assert.Equal(3, item));
         }
 
+        [Fact]
+        public void List_Is_Returned_As_The_Same_Type()
+        {
+            // Old versions of Append() and AppendRange() would
+            // always return the source list as an IList<T>().
+
+            // This test exists to ensure that there's no loss
+            // of type specificity.
+
+            // ReSharper disable once UnusedVariable
+            // ReSharper disable once SuggestVarOrType_Elsewhere
+            List<int> source = new List<int>()
+                .Append(0);
+
+            // ReSharper disable once UnusedVariable
+            // ReSharper disable once SuggestVarOrType_Elsewhere
+            MockList<int> source2 = new MockList<int>()
+                .Append(0);
+        }
+
+        private class MockList<T> : IList<T>
+        {
+            public IEnumerator<T> GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+
+            IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            public void Add(T item)
+            {
+            }
+
+            public void Clear()
+            {
+            }
+
+            public bool Contains(T item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void CopyTo(T[] array, int arrayIndex)
+            {
+            }
+
+            public bool Remove(T item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public int Count { get; }
+            public bool IsReadOnly { get; }
+            public int IndexOf(T item)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Insert(int index, T item)
+            {
+            }
+
+            public void RemoveAt(int index)
+            {
+            }
+
+            public T this[int index]
+            {
+                get { throw new NotImplementedException(); }
+                set { throw new NotImplementedException(); }
+            }
+        }
     }
 }
--- OneModel.Enumerables/IList/AppendExtensions.cs
+++ OneModel.Enumerables/IList/AppendExtensions.cs
@@ -10,7 +10,7 @@ namespace OneModel.Enumerables
         /// Appends a single item to the end of the list, 
         /// and returns the same list.
         /// </summary>
-        public static IList<T> Append<T>(this IList<T> list, T item)
+        public static TList Append<TList, TItem>(this TList list, TItem item) where TList : IList<TItem>
         {
             if (list == null)
             {
@@ -25,27 +25,7 @@ namespace OneModel.Enumerables
         /// Appends a sequence of items to the end of the list,
         /// and returns the same list.
         /// </summary>
-        public static IList<T> AppendRange<T>(this IList<T> list, IEnumerable<T> items)
-        {
-            if (list == null)
-            {
-                throw new ArgumentNullException(nameof(list));
-            }
-
-            if (items == null)
-            {
-                throw new ArgumentNullException(nameof(items));
-            }
-
-            list.AddRange(items);
-            return list;
-        }
-
-        /// <summary>
-        /// Appends a sequence of items to the end of the list,
-        /// and returns the same list.
-        /// </summary>
-        public static IList<T> AppendRange<T>(this IList<T> list, params T[] items)
+        public static TList AppendRange<TList, TItem>(this TList list, IEnumerable<TItem> items) where TList : IList<TItem>
         {
             if (list == null)
             {
