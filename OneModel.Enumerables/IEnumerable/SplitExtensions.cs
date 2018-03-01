12fa551464b13fb09476e68e214e775fd4a12dcd
Andrew Moore <andrew.moore@onemodel.co>
2018-03-01 10:48:08 -0600

    [2.1.5] Add `IEnumerable<T>.Split()` extension method.

---
+++ OneModel.Enumerables/IEnumerable/SplitExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OneModel.Enumerables
+{
+    public static class SplitExtensions
+    {
+        /// <summary>
+        /// Splits a sequence of items on items that match a predicate.
+        /// </summary>
+        public static IEnumerable<IReadOnlyList<T>> Split<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            var current = new List<T>();
+
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return current;
+                    current = new List<T>();
+                }
+                else
+                {
+                    current.Add(item);
+                }
+            }
+            
+            yield return current;
+        }
+    }
+}
